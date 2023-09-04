112a5cbe609ed642b0ee570cc93d34507e02799a
volte99 <mshreeshan@gmail.com>
2023-09-04 11:08:52 +0545

    added some basic routes

---
+++ client/src/pages/home/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div>Hi!!! I am Home</div>
+  )
+}
+
+export default index
---
+++ client/src/pages/index.js
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Layout from '@/components/layout'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return(
+   <>
+     <h1>
+      main page when you first open
+     </h1>
+  </> 
+  )
+}
