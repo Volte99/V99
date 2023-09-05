fa2f847de1dd792d0a1e79d48866c7b76fbdf3c2
Volte99 <mshreeshan@gmail.com>
2023-09-05 10:09:31 +0545

    gave default font family

--- client/src/pages/_document.js
+++ client/src/pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <style>
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Rubik+Iso&family=Rubik:wght@300;400&display=swap');
+        </style>
+        </style>
+      </Head>
       <body>
         <Main />
         <NextScript />
